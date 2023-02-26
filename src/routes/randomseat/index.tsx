764a9cfd58a1ae07364885df99e3e0f387b910a3
DongwonTuna <ttuna0790@gmail.com>
2023-02-26 19:46:52 +0900

    update

--- src/routes/randomseat/index.tsx
+++ src/routes/randomseat/index.tsx
@@ -1,7 +1,7 @@
 import { component$ } from '@builder.io/qwik';
-import { useTask$ } from '@builder.io/qwik';
+import { useBrowserVisibleTask$ } from '@builder.io/qwik';
 export default component$(() => {
-  useTask$(()=>{
+  useBrowserVisibleTask$(()=>{
         window.location.replace("https://randomseat.dongwontuna.net");
     })
   return (<></>)
--- src/routes/tviewchart/index.tsx
+++ src/routes/tviewchart/index.tsx
@@ -1,7 +1,7 @@
 import { component$ } from '@builder.io/qwik';
-import { useTask$ } from '@builder.io/qwik';
+import { useBrowserVisibleTask$ } from '@builder.io/qwik';
 export default component$(() => {
-  useTask$(()=>{
+  useBrowserVisibleTask$(()=>{
         window.location.replace("https://tviewchart.dongwontuna.net");
     })
   return (<></>)
